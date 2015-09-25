6ff8aefd311881053ac80b54e757056dabc64b0a
Rimon Hanna <rimonhanna@users.noreply.github.com>
2015-09-25 19:31:06 +0200

    Update 500px-slideshow.js

--- 500px-slideshow.js
+++ 500px-slideshow.js
@@ -26,8 +26,8 @@ var ikr500px = {
         this.populateLightboxContainer('body');
 
         jQuery(".ikr500px-lightbox-a").lightbox({
-            fileLoadingImage: "https://raw.githubusercontent.com/lokesh/lightbox2/master/src/images/loading.gif",
-            fileBottomNavCloseImage: "https://raw.githubusercontent.com/lokesh/lightbox2/master/src/images/close.png"
+            fileLoadingImage: "https://raw.githubusercontent.com/lokesh/lightbox2/master/dist/images/loading.gif",
+            fileBottomNavCloseImage: "https://raw.githubusercontent.com/lokesh/lightbox2/master/dist/images/close.png"
 
         });
 
