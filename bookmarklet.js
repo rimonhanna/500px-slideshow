2d60358effb696025b60539785861fdbac999f11
Rimon Hanna <rimonhanna@users.noreply.github.com>
2015-09-25 19:23:57 +0200

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -4,13 +4,13 @@
 
         $(link).attr({
             "rel": "stylesheet",
-            "href": "http://krechetov.net/~ikr/jquery-lightbox/css/lightbox.css",
+            "href": "https://raw.githubusercontent.com/lokesh/lightbox2/master/src/css/lightbox.css",
             "type": "text/css"
         });
     
         $("head").append(link);
 
-        $.getScript("http://krechetov.net/~ikr/jquery-lightbox/jquery.lightbox.js", function () {
+        $.getScript("https://raw.githubusercontent.com/lokesh/lightbox2/master/src/js/lightbox.js", function () {
             $.getScript("http://krechetov.net/~ikr/500px-slideshow/500px-slideshow.js", function () {
                 ikr500px.slideshow();
             });
